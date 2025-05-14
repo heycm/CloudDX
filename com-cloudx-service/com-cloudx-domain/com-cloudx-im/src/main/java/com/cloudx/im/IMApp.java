e9ed62537308b91acab6333d96e5d42ffd4f138b
heycm <heycm@qq.com>
2025-05-14 22:12:25 +0800

    feat: 构建IM服务

--- com-cloudx-service/com-cloudx-domain/com-cloudx-im/src/main/java/com/cloudx/im/IMApp.java
+++ com-cloudx-service/com-cloudx-domain/com-cloudx-im/src/main/java/com/cloudx/im/IMApp.java
@@ -1,9 +1,19 @@
 package com.cloudx.im;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * IM APP
  * @author heycm
  * @version 1.0
  * @since 2025/5/14 22:07
  */
-public class IMApp {}
+@SpringBootApplication
+public class IMApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMApp.class, args);
+    }
+
+}
